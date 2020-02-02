2725225c50d87c7344925a68192a6cb8c61a4f39
linlincheng <linlin.cheng@nyu.edu>
2020-02-02 18:49:25 -0500

    use fixture

--- test/test_SemiSupervisedTrainer.py
+++ test/test_SemiSupervisedTrainer.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import corextopic
+import pytest
 from hashtagger.dataProcessor import dataProcessor
 from hashtagger.modelTrainer import semiSupervisedTrainer
 
@@ -12,43 +13,48 @@ logging.basicConfig(
     )
 log = logging.getLogger('test_semiSupervisedTrainer')
 
-# remove exisiting test model folders
-for root, dirs, files in os.walk('./test/model', topdown=False):
-    for name in files:
-        os.remove(os.path.join(root, name))
-    for name in dirs:
-        os.rmdir(os.path.join(root, name))
+@pytest.fixture
+def DataProcessor():
+    DataProcessor = dataProcessor(data_path='./test/test_data.csv', response_field='content')
+    DataProcessor.get_text_data()
+    return(DataProcessor)
 
-"""
-if os.path.exists('./test/model'):
-    os.rmdir('./test/model')
-else:
-    log.info('The model foler does not exist')"""
-
-DataProcessor = dataProcessor(data_path='./test/test_data.csv', response_field='content')
-DataProcessor.get_text_data()
-
-SemiSupervisedTrainer = semiSupervisedTrainer(
-    words=DataProcessor.vocabulary,
-    doc_words=DataProcessor.doc_words,
-    n_topic=3,
-    save_model=True,
-    model_directory='test/model/',
-    print_words=True,
-    anchor_path='./test/test_anchor_words.json')
-SemiSupervisedTrainer.train_model()
-SemiSupervisedTrainer.save_model_object()
+@pytest.fixture
+def SemiSupervisedTrainer(DataProcessor):
+    SemiSupervisedTrainer = semiSupervisedTrainer(
+        words=DataProcessor.vocabulary,
+        doc_words=DataProcessor.doc_words,
+        n_topic=3,
+        save_model=True,
+        model_directory='./test/model/',
+        print_words=True,
+        anchor_path='./test/test_anchor_words.json')
+    SemiSupervisedTrainer.train_model()
+    SemiSupervisedTrainer.save_model_object()
+    return(SemiSupervisedTrainer)
 
 
-def test_semiSupervisedTrainer_train_model():
+def test_semiSupervisedTrainer_train_model(SemiSupervisedTrainer):
     log.info('testing semiSupervisedTrainer returns proper doc_words and vocabulary')
     assert isinstance(SemiSupervisedTrainer.topic_model, corextopic.corextopic.Corex), \
         'topic_model object type incorrect...'
 
 
-def test_semiSupervisedTrainer_save_model_object():
+def test_semiSupervisedTrainer_save_model_object(SemiSupervisedTrainer):
     log.info('testing semiSupervisedTrainer saves model objects properly')
     file_list_set = set(os.listdir(SemiSupervisedTrainer.model_object_path))
+    print(os.listdir(SemiSupervisedTrainer.model_object_path))
     expected_list_set = set(['model', 'words', 'anchor_words'])
     assert set(expected_list_set).issubset(file_list_set), 'save_model_objects not working properly, \
                                                            one or two files missing...'
+
+
+@pytest.fixture(scope="module")
+def remove_dir(model_dir='./test/model'):
+    yield
+    print('Deleting test model files...')
+    for root, dirs, files in os.walk(model_dir, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
--- test/test_UnSupervisedTrainer.py
+++ test/test_UnSupervisedTrainer.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import corextopic
+import pytest
 from hashtagger.dataProcessor import dataProcessor
 from hashtagger.modelTrainer import unSupervisedTrainer
 
@@ -12,42 +13,53 @@ logging.basicConfig(
     )
 log = logging.getLogger('test_unSupervisedTrainer')
 
-# remove exisiting test model folders
-for root, dirs, files in os.walk('./test/model', topdown=False):
-    for name in files:
-        os.remove(os.path.join(root, name))
-    for name in dirs:
-        os.rmdir(os.path.join(root, name))
-
 """if os.path.exists('./test/model'):
     os.rmdir('./test/model')
 else:
     log.info('The model foler does not exist')
 """
 
-DataProcessor = dataProcessor(data_path='./test/test_data.csv', response_field='content')
-DataProcessor.get_text_data()
+
+@pytest.fixture
+def DataProcessor():
+    DataProcessor = dataProcessor(data_path='./test/test_data.csv', response_field='content')
+    DataProcessor.get_text_data()
+    return(DataProcessor)
 
 
-UnSupervisedTrainer = unSupervisedTrainer(
-    words=DataProcessor.vocabulary,
-    doc_words=DataProcessor.doc_words,
-    n_topic=3, save_model=True,
-    model_directory='./test/model/',
-    print_words=True)
-UnSupervisedTrainer.train_model()
-UnSupervisedTrainer.save_model_object()
+@pytest.fixture
+def UnSupervisedTrainer(DataProcessor):
+    UnSupervisedTrainer = unSupervisedTrainer(
+        words=DataProcessor.vocabulary,
+        doc_words=DataProcessor.doc_words,
+        n_topic=3, save_model=True,
+        model_directory='./test/model/',
+        print_words=True)
+    UnSupervisedTrainer.train_model()
+    UnSupervisedTrainer.save_model_object()
+    return(UnSupervisedTrainer)
 
 
-def test_unSupervisedTrainer_train_model():
+def test_unSupervisedTrainer_train_model(UnSupervisedTrainer):
     log.info('testing unSupervisedTrainer returns proper doc_words and vocabulary')
     assert isinstance(UnSupervisedTrainer.topic_model, corextopic.corextopic.Corex), \
         'topic_model object type incorrect...'
 
 
-def test_unSupervisedTrainer_save_model_object():
+def test_unSupervisedTrainer_save_model_object(UnSupervisedTrainer):
     log.info('testing unSupervisedTrainer saves model objects properly')
     file_list_set = set(os.listdir(UnSupervisedTrainer.model_object_path))
     expected_list_set = set(['model', 'words'])
     assert set(expected_list_set).issubset(file_list_set), 'save_model_objects not working properly, \
                                                            one or two files missing...'
+
+
+@pytest.fixture(scope="module")
+def remove_dir(model_dir='./test/model'):
+    yield
+    print('Deleting test model files...')
+    for root, dirs, files in os.walk(model_dir, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
